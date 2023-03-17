81ae2bdf65baa224190fab5d1ca9425a0d6cf58e
Furkan Küçükali <furkankucukali@gmail.com>
2023-03-17 16:41:54 +0300

    first commit

---
+++ SenkronikAsenkronik/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SenkronikAsenkronik
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTopla_Click(object sender, EventArgs e)
+        {
+            Topla(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+        }
+
+        private void btnCarp_Click(object sender, EventArgs e)
+        {
+            Carp(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+        }
+        private void Topla(int sayi1,int sayi2)
+        {
+            MessageBox.Show($"Toplam : {sayi1 + sayi2}");
+        }
+        private void Carp(int sayi1, int sayi2)
+        {
+            Thread.Sleep(10000);
+            MessageBox.Show($"Carpim : {sayi1 * sayi2}");
+        }
+    }
+}
---
+++ WebApplication1/Models/CustomerContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class CustomerContext
+    {
+        public static List<Customer> Customers = new()
+        {
+            new Customer { FirstName = "Yavuz", LastName = "Kahraman ", Age = 27 },
+            new Customer { FirstName = "Oğuz", LastName = "Veli", Age = 23 }
+        };
+    }
+}

3e36107d0660cf5f68ab237f2ff731a5c7fc5643
Furkan Küçükali <furkankucukali@gmail.com>
2023-03-31 17:10:51 +0300

    'folder'

---
+++ WebApplication1/Controllers/FolderController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace WebApplication1.Controllers
+{
+    public class FolderController:Controller
+
+    {
+        public IActionResult List()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(),"wwwroot"));
+            var folders = directoryInfo.GetDirectories();
+            return View(folders);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string folderName) {
+
+            DirectoryInfo info = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(),"wwwroot",folderName ));
+            if(!info.Exists)
+            {
+                info.Create();
+            }
+            return RedirectToAction("List");
+        }
+    }
+}

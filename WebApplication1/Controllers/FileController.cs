a01015dd3456e53b19cb6b29e8ce44aa6f7c2b3c
Furkan Küçükali <furkankucukali@gmail.com>
2023-04-03 16:25:25 +0300

    File process completed

--- WebApplication1/Controllers/FileController.cs
+++ WebApplication1/Controllers/FileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
 
@@ -27,13 +28,50 @@ namespace WebApplication1.Controllers
             }
             return RedirectToAction("List");
         }
+        public IActionResult Remove(string fileName)
+        {
+            FileInfo fileInfo = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", fileName));
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+
+            }
+            return RedirectToAction("List")
+
+            return RedirectToAction("List");
+        }
         public IActionResult CreateWithData()
         {
             FileInfo fileInfo = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files",Guid.NewGuid().ToString()+".txt"));
             StreamWriter writer = fileInfo.CreateText();
             writer.Write("Merhaba ben furkan");
             writer.Close();
-            return RedirectToAction("List");
+            return RedirectToAction("List"); 
+        }
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Upload(IFormFile formFile)
+        {
+            if(formFile.ContentType == "image/png")
+            {
+                var ext = Path.GetExtension(formFile.FileName);
+                var path = Directory.GetCurrentDirectory() + "/wwwroot" + "/images/" + Guid.NewGuid() + ext;
+                FileStream stream = new FileStream(path, FileMode.Create);
+                formFile.CopyTo(stream);
+                TempData["message"] = "Dosya upload başarı ile gerçekleşti";
+
+            }
+            else
+            {
+                TempData["message"] = "Dosya upload edilemedi, uygunsuz dosya tipi";
+
+            }
+
+
+            return RedirectToAction("Upload");
         }
     }
 }

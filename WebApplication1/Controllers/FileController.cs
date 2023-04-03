73eb6eea309e07268283ef0cfa77508f9986527b
Furkan Küçükali <furkankucukali@gmail.com>
2023-04-03 10:01:41 +0300

    file transactions

---
+++ WebApplication1/Controllers/FileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace WebApplication1.Controllers
+{
+    public class FileController : Controller
+    {
+        public IActionResult List()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(),"wwwroot","files"));
+            var files = directoryInfo.GetFiles();
+            return View(files);
+        }
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string fileName)
+        {
+            FileInfo fileInfo = new FileInfo(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files",fileName));
+            if(!fileInfo.Exists)
+            {
+                fileInfo.Create();
+            }
+            return RedirectToAction("List");
+        }
+    }
+}

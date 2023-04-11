54cf59ee4f16322c0b46ab270eff3ea1df0303df
Furkan Küçükali <furkankucukali@gmail.com>
2023-04-11 16:37:10 +0300

    validation

--- WebApplication1/Models/Customer.cs
+++ WebApplication1/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,8 +10,13 @@ namespace WebApplication1.Models
     {
 
         public int Id { get; set; }
+        [Required(ErrorMessage ="Ad alanı boş geçilemez.")]
+        [MaxLength(30,ErrorMessage ="Ad alanı en fazla 30 karakter olabilir.")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Soyad alanı boş geçilemez.")]
+        [MinLength(3, ErrorMessage = "Soyad alanı en az 3 karakter olabilir.")]
         public string  LastName { get; set; }
+        [Range(18,40,ErrorMessage = "En az 18 ve En fazla 40 olabilir.")]
         public int Age { get; set; }
     }
 }
